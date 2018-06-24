c49def71843568635afe19be1d857c7022a09a75
Vadims Petrusevs <pvman@inbox.ru>
2018-06-24 18:10:20 +0200

    New method to easy get order items

--- src/ModuleOrders.php
+++ src/ModuleOrders.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace TMCms\Modules\Orders;
 
 use TMCms\Modules\IModule;
+use TMCms\Modules\Orders\Entity\OrderEntity;
 use TMCms\Modules\Orders\Entity\OrderEntityRepository;
+use TMCms\Modules\Orders\Entity\OrderItemEntityRepository;
 use TMCms\Traits\singletonInstanceTrait;
 
 \defined('INC') or exit;
@@ -37,5 +39,18 @@ class ModuleOrders implements IModule
         $orders->addOrderByField('ts_issued', true);
 
         return $orders;
+    }
+
+    /**
+     * @param OrderEntity $order
+     *
+     * @return OrderItemEntityRepository
+     */
+    public static function getOrderItems(OrderEntity $order): OrderItemEntityRepository
+    {
+        $items = new OrderItemEntityRepository();
+        $items->setWhereOrderId($order->getId());
+
+        return $items;
     }
 }
